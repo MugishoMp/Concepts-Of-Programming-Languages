812c09559b29d71c140bebb6ea5c7fbabca24ae9
mugisho <mugishompozi@gmail.com>
2023-10-21 13:01:47 +0200

    add file info

--- include/custom_headers.h
+++ include/custom_headers.h
@@ -1,3 +1,12 @@
+/**
+ * @file custom_headers.h
+ * @author Mugisho Mpozi s2440547
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-20
+ * 
+ */
+
 #ifndef CUSTOM_HEADERS_H
 #define CUSTOM_HEADERS_H
 
